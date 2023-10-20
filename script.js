60c05836bdeccfc94f26f7c29e84d095d3c0aa38
Jovanny De Leon <roanlandao05@gmail.com>
2023-10-20 11:08:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// Get a reference to the button element
+var button1 = document.getElementById("btn1");
+
+// Add a click event listener to the button
+button1.addEventListener("click", function() {
+    // Specify the URL you want to redirect to
+    var redirectTo = "coc1.html";
+
+    // Redirect to the specified URL
+    window.location.href = redirectTo;
+});
+
+var button2 = document.getElementById("btn2");
+
+// Add a click event listener to the button
+button2.addEventListener("click", function() {
+    // Specify the URL you want to redirect to
+    var redirectTo = "coc2";
+
+    // Redirect to the specified URL
+    window.location.href = redirectTo;
+});
+
+var button3 = document.getElementById("btn3");
+
+// Add a click event listener to the button
+button3.addEventListener("click", function() {
+    // Specify the URL you want to redirect to
+    var redirectTo = "coc3";
+
+    // Redirect to the specified URL
+    window.location.href = redirectTo;
+});
+
+var button4 = document.getElementById("btn4");
+
+// Add a click event listener to the button
+button4.addEventListener("click", function() {
+    // Specify the URL you want to redirect to
+    var redirectTo = "coc4";
+
+    // Redirect to the specified URL
+    window.location.href = redirectTo;
+});
